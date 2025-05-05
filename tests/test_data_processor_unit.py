452fd2f0470741b2859379cb44d5ba0681c29cdf
Ariel Berardi <ariel.berardi95@gmail.com>
2025-05-05 17:18:43 +0100

    Refactor mock setup in DPU tests (#5)

--- tests/test_data_processor_unit.py
+++ tests/test_data_processor_unit.py
@@ -8,70 +8,44 @@ from quantari.data_processor_unit import DataProcessorUnit
 
 class TestDataProcessorUnit:
     @pytest.fixture
-    def data_processor_unit(self):
-        return DataProcessorUnit()
+    @patch("quantari.data_processor_unit.KafkaClient")
+    @patch("quantari.data_processor_unit.TimescaleClient")
+    def data_processor_unit(
+        self,
+        mock_timescale_client,
+        mock_kafka_client,
+    ):
+        mock_timescale_client.return_value = MagicMock()
+        mock_kafka_client.return_value = MagicMock()
 
-    @pytest.fixture
-    def mock_kraken_client(self):
-        with patch(
-            "quantari.data_processor_unit.SpotWSClient", new_callable=AsyncMock
-        ) as mock_client:
-            yield mock_client
+        dpu = DataProcessorUnit()
+        dpu.kraken_client = AsyncMock()
 
-    @pytest.fixture
-    def mock_kafka_client(self):
-        with patch(
-            "quantari.data_processor_unit.KafkaClient", new_callable=MagicMock
-        ) as mock_client:
-            yield mock_client
-
-    @pytest.fixture
-    def mock_timescale_client(self):
-        with patch(
-            "quantari.data_processor_unit.TimescaleClient", new_callable=MagicMock
-        ) as mock_client:
-            yield mock_client
+        return dpu
 
     @pytest.mark.asyncio
     async def test_close(
         self,
         data_processor_unit,
-        mock_kraken_client,
-        mock_kafka_client,
-        mock_timescale_client,
     ):
-        data_processor_unit.kraken_client = mock_kraken_client
-        data_processor_unit.kafka_producer = mock_kafka_client
-        data_processor_unit.db_client = mock_timescale_client
-
         await data_processor_unit.close()
 
-        mock_kraken_client.close.assert_awaited_once()
-        mock_kafka_client.close_producer.assert_called_once()
-        mock_timescale_client.close_connection.assert_called_once()
+        data_processor_unit.kraken_client.close.assert_awaited_once()
+        data_processor_unit.kafka_producer.close_producer.assert_called_once()
+        data_processor_unit.db_client.close_connection.assert_called_once()
 
-    def test_process_market_data_on_candle_closure(
-        self, data_processor_unit, mock_kafka_client, mock_timescale_client
-    ):
-        data_processor_unit.kafka_producer = mock_kafka_client
-        data_processor_unit.db_client = mock_timescale_client
-
+    def test_process_market_data_on_candle_closure(self, data_processor_unit):
         data_processor_unit.last_candle_data = {"interval_begin": "2023-01-01T00:00:00"}
         new_data = {"interval_begin": "2023-01-01T00:01:00"}
 
         data_processor_unit.process_market_data(new_data)
 
-        mock_kafka_client.publish_market_data.assert_called_once()
-        mock_timescale_client.save_market_data.assert_called_once()
+        data_processor_unit.kafka_producer.publish_market_data.assert_called_once()
+        data_processor_unit.db_client.save_market_data.assert_called_once()
 
         assert data_processor_unit.last_candle_data == new_data
 
-    def test_process_market_data_no_candle_closure(
-        self, data_processor_unit, mock_kafka_client, mock_timescale_client
-    ):
-        data_processor_unit.kafka_producer = mock_kafka_client
-        data_processor_unit.db_client = mock_timescale_client
-
+    def test_process_market_data_no_candle_closure(self, data_processor_unit):
         data_processor_unit.last_candle_data = {
             "volume": "1",
             "interval_begin": "2023-01-01T00:00:00",
@@ -81,8 +55,8 @@ class TestDataProcessorUnit:
 
         data_processor_unit.process_market_data(new_data)
 
-        mock_kafka_client.publish_market_data.assert_not_called()
-        mock_timescale_client.save_market_data.assert_not_called()
+        data_processor_unit.kafka_producer.publish_market_data.assert_not_called()
+        data_processor_unit.db_client.save_market_data.assert_not_called()
 
         assert data_processor_unit.last_candle_data == new_data
 
@@ -109,18 +83,13 @@ class TestDataProcessorUnit:
         self,
         mock_spot_ws_client,
         data_processor_unit,
-        mock_kafka_client,
-        mock_timescale_client,
     ):
+        # Setup Mock for Kraken Websocket Client
         mock_kraken_instance = AsyncMock()
         mock_kraken_instance.start = AsyncMock()
         mock_kraken_instance.subscribe = AsyncMock()
         mock_kraken_instance.exception_occur = False
         mock_spot_ws_client.return_value = mock_kraken_instance
-
-        # data_processor_unit.kraken_client = mock_kraken_client
-        data_processor_unit.kafka_producer = mock_kafka_client
-        data_processor_unit.db_client = mock_timescale_client
 
         # Simulate the exception occurring after a short delay
         async def stop_after_delay():
@@ -132,10 +101,10 @@ class TestDataProcessorUnit:
         await data_processor_unit.run()
 
         # Setup Database Connection
-        mock_timescale_client.create_market_table.assert_called_once()
+        data_processor_unit.db_client.create_market_table.assert_called_once()
 
         # Setup Kafka producer
-        mock_kafka_client.create_producer.assert_called_once()
+        data_processor_unit.kafka_producer.create_producer.assert_called_once()
 
         # Start and subscribe to Kraken Websocket
         mock_kraken_instance.start.assert_awaited_once()
